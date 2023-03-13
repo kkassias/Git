409f23242fc593108c0c05a9d49d6babc572529d
kkassias <kassilva.estudos@gmail.com>
2023-03-13 10:57:51 -0300

    Adicionanod um console log

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-consol.log("Rodando o sistema de teste de um projeto")
+console.log("Rodando o sistema de teste de um projeto")
 
 // mais um consol.log de teste
-consol.log("Teste de sincronização git e github")
+console.log("Teste de sincronização git e github")
+console.log("Enviando para o github")
+
